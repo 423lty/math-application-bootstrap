438b082a7766a667b7240ac852f87c1d24ec6bf0
haz <158719178+423lty@users.noreply.github.com>
2025-08-21 19:38:01 +0900

    25/08/21

--- src/app.js
+++ src/app.js
@@ -5,12 +5,22 @@ const applicationState = Object.freeze({
     finish: "finish",
 });
 
+const applicationMathLevel = Object.freeze({
+    highSchool: "hs",
+    university: "univ"
+})
+
+
 /**
  * 実行クラス
  */
 class App {
 
+    // 現在の状態
     #state = applicationState.title;
+
+    //数学の難易度
+    #mathLevel = "";
 
     /**高校数学の情報を格納する変数 */
     static hsJsonDatas = [];
@@ -25,7 +35,6 @@ class App {
 
         //現在の状態を取得反映
         this.checkState();
-
     }
 
     /**実行中の処理 */
@@ -45,12 +54,19 @@ class App {
                 break;
 
         }
+
+        this.checkState();
     }
 
     /**状態のチェック */
     checkState() {
         // アプリケーション内のsection要素をすべて取得
         const appSections = Array.from(document.querySelectorAll("section"));
+
+        //全てを一回表示状態にする
+        appSections.forEach(section => {
+            section.style.display = "block";
+        })
 
         /**stateと不一致するオブジェクトを取得 */
         const disActiveStates = appSections.filter(item => item.className != this.#state);
@@ -59,24 +75,52 @@ class App {
         disActiveStates.forEach(disActiveState => {
             disActiveState.style.display = "none";
         })
+
+        console.log("現在表示state:" + this.#state);
     }
 
     /**タイトル画面の更新処理 */
     titleUpdate() {
+
+        // タイトルのオブジェクトを全て取得
+        const titleSection = document.querySelector(".title");
+        const titleSectionObjects = titleSection.querySelectorAll("div");
+
+        //どちらの数学をするか選択
+        titleSectionObjects.forEach(object => {
+            object.addEventListener("click", () => {
+                //レベルの選択
+                this.#mathLevel = object.className;
+
+                //状態の遷移
+                this.#state = applicationState.levelSelect;
+
+                //レベルレベル
+                const levelSelectTitleText = document.querySelector(".levelSelect").querySelector("h2");
+
+                // levelSelectの表示タイトルの変更
+                if (this.#mathLevel == applicationMathLevel.highSchool)
+                    levelSelectTitleText.textContent = "高校数学";
+                else if (this.#mathLevel == applicationMathLevel.university)
+                    levelSelectTitleText.textContent = "大学数学";
+            })
+        });
+
+        this.checkState();
     }
 
     /**難易度の選択更新処理 */
-    levelSelectUpdate(){
+    levelSelectUpdate() {
 
     }
 
     /**問題回答処理の更新処理 */
-    problemAnswerUpdate(){
+    problemAnswerUpdate() {
 
     }
 
     /**終了時の更新処理 */
-    finishUpdate(){
+    finishUpdate() {
 
     }
 
